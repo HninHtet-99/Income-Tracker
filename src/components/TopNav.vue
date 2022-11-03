ae974e4ab5a853625d732d3b33c21da98a99963d
HninHtet-99 <hninhtet1113@gmail.com>
2022-11-03 21:39:37 +0630

    adding style

---
+++ src/components/TopNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="title">
+        <h1>Home</h1>
+        <div>
+            <input type="text">
+            <button class="btn">Add</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.title{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <TopNav></TopNav>
+  </div>
+</template>
+
+<script>
+import TopNav from '../components/TopNav'
+export default {
+  components: { TopNav },
+
+}
+</script>
+
+<style>
+
+</style>
